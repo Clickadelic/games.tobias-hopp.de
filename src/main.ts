1de81a59174d5075b710382e2faeea024a1e2b65
Clickadelic <toby.hopp@gmail.com>
2026-04-07 07:01:33 +0200

    TS strict = false, assets unzipped

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import "./style.css";
 
-const canvas = document.getElementById("game") as HTMLCanvasElement;
+const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+const c = canvas.getContext("2d");
+
 // Experimenting with canvas size and styles
 canvas.width = 1024;
 canvas.height = 576;
-canvas.style.backgroundColor = "black";
-canvas.style.display = "block";
-canvas.style.margin = "0 auto";
 
-const ctx = canvas.getContext("2d");
-
-ctx?.fillRect(10, 10, 50, 50);
+c.fillStyle = "white";
+c?.fillRect(0, 0, canvas.width, canvas.height);
+c.fillStyle = "red";
+c?.fillRect(200, 100, 100, 100);
